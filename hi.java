91defdb90837508581d3f9b400b7ed297fe7c3ae
TanmayBhakta329 <103892781+TanmayBhakta329@users.noreply.github.com>
2023-06-14 21:28:21 +0530

    5th removed

--- hi.java
+++ hi.java
@@ -5,7 +5,6 @@ public class Main
 		System.out.println("2nd line");
 		System.out.println("3rd line");
 		System.out.println("4th line");
-		System.out.println("5th line");
 		System.out.println("6th line added");	
 		System.out.println("7th line added");
 		System.out.println("8th line added");
