2ba21f43935f911d0c8b032a6804572bc093763a
TanmayBhakta329 <103892781+TanmayBhakta329@users.noreply.github.com>
2023-06-14 21:16:33 +0530

    10th

--- hi.java
+++ hi.java
@@ -1,7 +1,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World!!");
 		System.out.println("2nd line");
 		System.out.println("3rd line");
 		System.out.println("4th line");
